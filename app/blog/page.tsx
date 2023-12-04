0ee6a5836c84d847f77885fe5e8caa0a1f008706
mochelbeb <mchelbeb13@gmail.com>
2023-12-05 01:53:17 +0300

    feat(blog): update posts list style

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,12 @@
 import { readMdFile } from "@/utils/md";
 import { getPublicPath } from "@/utils/utils";
 import { blogMatterSchema } from "@/validation/blog";
+import dayjs from "dayjs";
+import advancedFormat from "dayjs/plugin/advancedFormat";
 import { readdirSync } from "fs";
 import { Metadata } from "next";
 import Link from "next/link";
-
+dayjs.extend(advancedFormat);
 export const metadata: Metadata = {
   title: "Blog | Islam Naasani",
   description:
@@ -24,17 +26,33 @@ export default async function Page() {
     .filter(
       (post) => post.draft !== true || process.env.NODE_ENV === "development",
     )
-    .sort((a, b) => (a.publishedAt > b.publishedAt ? 1 : -1));
+    .sort((a, b) => (a.publishedAt < b.publishedAt ? 1 : -1));
   return (
     <>
-      <div className="mx-12 my-20 mt-32 flex flex-col items-center text-2xl sm:text-3xl">
-        <h2 className="mb-20 text-5xl sm:text-7xl">Blog</h2>
-        <ol className="list-disc">
-          {parsed.map((post) => (
+      <div className="mx-auto my-20 mt-32 flex max-w-2xl flex-col items-center gap-3 px-8">
+        <h1 className="text-5xl sm:text-7xl">Blog</h1>
+        <p className="text-center text-lg">
+          {`My personal Blog, I write about problems I've faced or new things
+          I've learned.`}
+        </p>
+        <ol className="mt-10 flex flex-col gap-5">
+          {parsed.map((post, i) => (
             <li key={post.slug}>
-              <Link href={`/blog/${post.slug}`} className="underline">
-                {post.title}
-                {post.draft && " (draft)"}
+              {i !== 0 && (
+                <hr className="mb-5 h-0.5 rounded-sm bg-purple-200" />
+              )}
+              <Link href={`/blog/${post.slug}`}>
+                <h2 className="text-2xl text-violet-400">
+                  {post.title}
+                  {post.draft && " (draft)"}
+                </h2>
+                <time
+                  className="text-sm text-gray-300"
+                  dateTime={dayjs(post.publishedAt).format("YYYY-MM-DD")}
+                >
+                  {dayjs(post.publishedAt).format("MMM Do, YYYY")}
+                </time>
+                <p className="mt-2">{post.summary}</p>
               </Link>
             </li>
           ))}
--- constants/navigation.ts
+++ constants/navigation.ts
@@ -2,10 +2,15 @@ export const navigation = [
   {
     name: "Islam Naasani",
     href: "/",
-    color: "rgb(229 101 255)",
+    color: "rgb(0 182 133)",
     startWith: false,
   },
-  { name: "Blog", href: "/blog", color: "rgb(0 182 133)", startWith: true },
+  {
+    name: "Blog",
+    href: "/blog",
+    color: "rgb(167 139 250)",
+    startWith: true,
+  },
   {
     name: "Projects",
     href: "/projects",
